2f5226b8c4310571772f89ae87def4ebbd4c3f7c
Migwee <aidan.king1999@hotmil.co.uk>
2014-02-05 10:11:02 +0000

    Create string.py

---
+++ string.py
@@ -0,0 +1,5 @@
+firstLine = "This is the rhythm of the night,\nThe night.\n"
+secondLine = "Oh yeah!."
+print(firstLine + secondLine)
+#expected outcome is This is the rhythm of the night, The night
+#Oh yeah!
