31c2144497c9f142391a171ea48bb8759191a14d
Migwee <aidan.king1999@hotmil.co.uk>
2014-02-05 10:21:54 +0000

    Create sentence.py

---
+++ sentence.py
@@ -0,0 +1,8 @@
+start = "Hello, "
+statement = "You are the chosen one! "
+statement2 = "You are a filthy, fat hobbit! "
+statement3 = "Potato, Boil 'em mash 'em stick 'em in a stew!"
+statement4 = "No! Fat hobbits! "
+sentence = start + statement + statement2 + statement3 + statement4
+print(sentence)
+#Lord of the Rings quote
