619fdc12ab1eae3af8efab261c5aad0bc9f279c7
Migwee <aidan.king1999@hotmil.co.uk>
2014-02-05 10:27:44 +0000

    Create songtest.py

---
+++ songtest.py
@@ -0,0 +1,6 @@
+firstLine = "Now, this is a story all about how\n"
+secondLine = "My life got flipped-turned upside down\n"
+thirdLine = "And I'd like to take a minute\n"
+fourthLine = "Just sit right there\n"
+fithLine = "I'll tell you how I became the prince of a town called Bel Air!\n"
+print(firstLine + secondLine + thirdLine + fourthLine + fifthLine)
